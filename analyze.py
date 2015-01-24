b2632732d1256225937df1dacd7de4529585cee2
Thomas Schneider <maxmusterm@gmail.com>
2015-01-24 05:11:20 +0100

    analyze

--- analyze.py
+++ analyze.py
@@ -2,12 +2,16 @@
 import openvgdb
 import sys
 
-vgdb=openvgdb.vgdb(sys.argv[1])
-systems=vgdb.get_systems()
-for system in systems:
-    print str(system) +" "+ systems[system]
+if len(sys.argv)>1:
+    vgdb=openvgdb.vgdb(sys.argv[1])
 
-print "Fields you get:"
-game=vgdb.get_console_fg(sys.argv[2])
-for field in game:
-    print field+" -> "+str(game[field])
+if len(sys.argv)==2:
+    systems=vgdb.get_systems()
+    for system in systems:
+        print str(system) +" "+ systems[system]
+elif len(sys.argv)==3:
+
+    print "Fields you get:"
+    game=vgdb.get_console_fg(sys.argv[2])
+    for field in game:
+        print field+" -> "+str(game[field])
