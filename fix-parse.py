891a923a3946850291ca209484f5be289939528e
Thomas Schneider <maxmusterm@gmail.com>
2015-01-28 01:30:11 +0100

    even thought my script was cleaner before... this is less work for the poor converter guys

--- fix-parse.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python2
-import re,sys
-
-f=open(sys.argv[1],'r').read()
-w=open(sys.argv[2],'w')
-old=chr(int('0A',16))
-new=chr(int('0D',16))+chr(int('0A',16))
-w.write(re.sub('('+old+')',new,f))
-w.close()
--- generate-libretrodat.py
+++ generate-libretrodat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys,ast
+import sys,ast,re
 import openvgdb
 
 string='{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName","description":"romExtensionlessFileName"}}'
@@ -9,31 +9,43 @@ games=[]
 game=[]
 val='game'
 
-def parse(a,level):
+def fix(foo):
+    old=chr(int('0A',16))
+    new=chr(int('0D',16))+chr(int('0A',16))
+    return re.sub('('+old+')',new,foo)
+
+def parse(a,level,nnl=False):
     ret=[]
     last=[]
     check=['crc','md5','sha1','serial']
     for entry in a:
         if type(a[entry])==type({}) and entry=="game":
-            last.append("\t"*level+str(entry)+" ( ")
+            last.append("\t"*level+str(entry)+" (")
             last.append(parse(a[entry],level+1))
             last.append("\t"*level+")\n")
         elif type(a[entry])==type({}) and entry=="rom":
-            last.append(" "*level+str(entry)+" ( ")
-            last.append(parse(a[entry],level)[:-1])
-#            print parse(a[entry],level+1)[:-1]
-            last.append(" "*level+")\n")
+            last.append("\t%s ( " % (str(entry)) )
+            last.append(parse(a[entry],level,True))
+            last.append(")\n")
         else:
             value=game[a[entry]]
             if type(value)==type(42) or entry in check:
-                ret.append("\t"*level+str(entry)+" "+str(value)+" ")
+                if nnl==False:
+                    ret.append("\t"*level+str(entry)+" "+str(value)+" ")
+                else:
+                    ret.append(str(entry)+" "+str(value)+" ")
             else:
                 try:
-                    ret.append("\t"*level+str(entry)+' "'+str(value).encode('utf-8')+"\" ")
+                    if nnl==False:
+                        ret.append("\t"*level+str(entry)+' "'+str(value).encode('utf-8')+"\" ")
+                    else:
+                        ret.append(str(entry)+' "'+str(value).encode('utf-8')+"\" ")
                 except:
                     print 'Couldn\'t parse'
                     print value
-        ret.append("\n")
+        if nnl==False:
+            #check if we need a newline
+            ret.append("\n")
     return "".join(ret)+"".join(last)
 
 if len(sys.argv)%2!=0:
@@ -42,8 +54,8 @@ if len(sys.argv)%2!=0:
 
 O=openvgdb.vgdb('openvgdb.sqlite')
 print 'Getting the game list.This can take a while...'
-header="clrmamepro ( \n\
-name \""+O.get_systems()[int(sys.argv[1])]+"\"\n\
+header="clrmamepro (\n\
+\tname \""+O.get_systems()[int(sys.argv[1])]+"\"\n\
 )\n"
 games=O.get_console(sys.argv[1])
 print 'Done.Starting to convert'
@@ -58,8 +70,8 @@ for i in range(0,(len(sys.argv)-2)/2):
     par=ast.literal_eval(string)
     f=open(sys.argv[3+i*2],"w")
     print 'writing '+sys.argv[3+i*2]
-    f.write(header)
+    f.write(fix(header))
     for game in games:
-        f.write(parse(par,0))
+        f.write(fix(parse(par,0)))
     f.close()
 print 'Done everything went according to plan'
