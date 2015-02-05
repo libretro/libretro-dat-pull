e202e8ce36069bf661d90710451e50f255b02601
twinaphex <libretro@gmail.com>
2015-02-05 16:42:35 +0100

    Update SQLite DB

--- libretro-dat-pull.sh
+++ libretro-dat-pull.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #./generate-dat.py 38 '{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName"}}' "Sony - PlayStation"
-#./generate-dat.py 2 '{"game":{"rom":{"name":"romFileName","size":"romSize","md5":"romHashMD5","crc":"romHashCRC","sha1":"romHashSHA1"},"name":"romExtensionlessFileName"}}' "Arcade"
+./generate-dat.py 2 '{"game":{"rom":{"name":"romFileName","size":"romSize","md5":"romHashMD5","crc":"romHashCRC","sha1":"romHashSHA1"},"name":"releaseTitleName"}}' "Arcade"
