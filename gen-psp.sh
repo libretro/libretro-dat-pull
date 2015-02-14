1163c75212775a01073ccbb2f7ff360f31251fae
twinaphex <libretro@gmail.com>
2015-02-14 04:46:59 +0100

    Refactor N64 somewhat

--- gen-psp.sh
+++ gen-psp.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-pypy generate-dat.py -s 39 '{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName"}}' '{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName","developer":"releaseDeveloper"}}' '{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName","publisher":"releasePublisher"}}' '{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName","releaseyear":"Year"}}' '{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName","releasemonth":"Month"}}' psp
-mv psp_1.dat "dat/$psp.dat"
-mv psp_2.dat "metadat/developer/$psp.dat"
-mv psp_3.dat "metadat/publisher/$psp.dat"
-mv psp_4.dat "metadat/releaseyear/$psp.dat"
-mv psp_5.dat "metadat/releasemonth/$psp.dat"
+pypy generate-dat.py -s 39 '{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName","developer":"releaseDeveloper"}}' '{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName","publisher":"releasePublisher"}}' '{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName","releaseyear":"Year"}}' '{"game":{"rom":{"serial":"romSerial","image":"romFileName"},"name":"romExtensionlessFileName","releasemonth":"Month"}}' psp
+mv psp_1.dat "metadat/developer/test.dat"
+mv psp_2.dat "metadat/publisher/test.dat"
+mv psp_3.dat "metadat/releaseyear/test.dat"
+mv psp_4.dat "metadat/releasemonth/test.dat"
 
--- scripts/Nintendo 64.sh
+++ scripts/Nintendo 64.sh
@@ -4,9 +4,14 @@ SYSTEMID_N64=23
 ABBREV_SYSTEMNAME_N64="N64"
 SYSTEMNAME_N64="Nintendo - Nintendo 64"
 
+GEN_DEVELOPER_N64='{"game":{"rom":{"crc":"romHashCRC"},"name":"romExtensionlessFileName","developer":"releaseDeveloper"}}'
+GEN_PUBLISHER_N64='{"game":{"rom":{"crc":"romHashCRC"},"name":"romExtensionlessFileName","publisher":"releasePublisher"}}'
+GEN_RELEASEYEAR_N64='{"game":{"rom":{"crc":"romHashCRC"},"name":"romExtensionlessFileName","releaseyear":"Year"}}'
+GEN_RELEASEMONTH_N64='{"game":{"rom":{"crc":"romHashCRC"},"name":"romExtensionlessFileName","releasemonth":"Month"}}'
+
 _gen_developer_n64() {
    echo "--- ${2} - DAT Developer"
-   $PYTHON ./generate-dat.py -s ${3} '{"game":{"rom":{"crc":"romHashCRC"},"name":"romExtensionlessFileName","developer":"releaseDeveloper"}}' "${1}"
+   $PYTHON ./generate-dat.py -s ${3} ${GEN_DEVELOPER_N64} "${1}"
    mv "${1}.dat" "metadat/developer/${2}.dat"
 }
 
@@ -16,7 +21,7 @@ gen_developer_n64() {
 
 _gen_publisher_n64() {
    echo "--- ${2} - DAT Publisher"
-   $PYTHON ./generate-dat.py -s ${3} '{"game":{"rom":{"crc":"romHashCRC"},"name":"romExtensionlessFileName","publisher":"releasePublisher"}}' "${1}"
+   $PYTHON ./generate-dat.py -s ${3} ${GEN_PUBLISHER_N64} "${1}"
    mv "${1}.dat" "metadat/publisher/${2}.dat"
 }
 
@@ -26,7 +31,7 @@ gen_publisher_n64() {
 
 _gen_releaseyear_n64() {
    echo "--- ${2} - DAT Releaseyear"
-   $PYTHON ./generate-dat.py -s ${3} '{"game":{"rom":{"crc":"romHashCRC"},"name":"romExtensionlessFileName","releaseyear":"Year"}}' "${1}"
+   $PYTHON ./generate-dat.py -s ${3} ${GEN_RELEASEYEAR_N64} "${1}"
    mv "${1}.dat" "metadat/releaseyear/${2}.dat"
 }
 
@@ -36,7 +41,7 @@ gen_releaseyear_n64() {
 
 _gen_releasemonth_n64() {
    echo "--- ${2} - DAT Releasemonth"
-   $PYTHON ./generate-dat.py -s ${3} '{"game":{"rom":{"crc":"romHashCRC"},"name":"romExtensionlessFileName","releasemonth":"Month"}}' "${1}"
+   $PYTHON ./generate-dat.py -s ${3} ${GEN_RELEASEMONTH_N64} "${1}"
    mv "${1}.dat" "metadat/releasemonth/${2}.dat"
 }
 
@@ -44,10 +49,20 @@ gen_releasemonth_n64() {
    _gen_releasemonth_n64 "${ABBREV_SYSTEMNAME_N64}" "${SYSTEMNAME_N64}" ${SYSTEMID_N64}
 }
 
-gen_n64() {
-   #gen_dat_n64
+_gen_n64() {
+   #DOESN'T WORK
+   #$PYTHON ./generate-dat.py -s ${3} "${GEN_DEVELOPER_N64}" "${GEN_PUBLISHER_N64}" "${GEN_RELEASEYEAR_N64}" "${GEN_RELEASEMONTH_N64}" "${1}"
+   #mv "${1}_1.dat" "metadat/developer/${2}.dat"
+   #mv "${1}_2.dat" "metadat/publisher/${2}.dat"
+   #mv "${1}_3.dat" "metadat/releaseyear/${2}.dat"
+   #mv "${1}_4.dat" "metadat/releasemonth/${2}.dat"
    gen_developer_n64
    gen_publisher_n64
    gen_releaseyear_n64
    gen_releasemonth_n64
 }
+
+gen_n64() {
+   _gen_n64 "${ABBREV_SYSTEMNAME_N64}" "${SYSTEMNAME_N64}" ${SYSTEMID_N64}
+}
+
