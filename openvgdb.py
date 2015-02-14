5901c2135a49108db228c2a31eecea7e7ff879f8
Thomas Schneider <maxmusterm@gmail.com>
2015-02-14 21:55:06 +0100

    New month TBA introduced

--- openvgdb.py
+++ openvgdb.py
@@ -4,7 +4,7 @@ import sqlite3 as lite
 import sys,re,time
 
 months={'January':1,'February':2,'March':3,'April':4,'May':5,'June':6,'July':7,'August':8,'September':9,'October':10,'November':11,'December':12}
-mon={'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
+mon={'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12,'TBA':None}
 debug=0
 
 def parse_date(taaag):
